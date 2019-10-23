bed2c86c8ffbc48dd4b8763d47f4a057095a759a
Dan Erbland <danerbland@gmail.com>
2019-10-23 15:11:54 -0400

    implemented log(n) binary-search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,22 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length === 1){
+		return array[0] === target
+	}
+
+	let midpoint = Math.floor(array.length/2)
+
+	if(array[midpoint] === target){
+		return true;
+	}
+
+	if(array[midpoint]>target){
+		return binarySearch(array.slice(0,midpoint), target)
+	} else {
+		return binarySearch(array.slice(midpoint), target)
+	}
+
 };
 
 /*
