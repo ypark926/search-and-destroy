fbf8ee38327252efa14075577d4a21d8bff1af72
Milo Calvo-Platero <macp92@gmail.com>
2019-10-23 16:21:10 -0400

    jump around jump around, jump up jump up and get down

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -2,7 +2,28 @@
 
 // Complete this algo
 const minJumps = arr => {
-
+  let i = 0;
+  let counter = 0;
+  while (i < arr.length - 1){
+    if (arr[i]+i >= arr.length - 1){
+      return ++counter
+    }
+    i = helper(i, arr)
+    counter++
+  }
+  return counter
 };
+
+function helper(index, array){
+  let largest = 0;
+  let largestIndex = 0;
+  for (let i = 1; i <= array[index]; i++){
+    if (i + array[index+i] > largest){
+      largest = i +array[index+i]
+      largestIndex = index+i
+    }
+  }
+  return largestIndex
+}
 
 module.exports = minJumps
